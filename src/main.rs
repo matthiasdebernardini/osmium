55756e68b6ded9d359b9e0e118ece978559c067b
matthiasdebernardini <m.f.debern@protonmail.com>
2022-12-31 20:05:40 +0100

    Fleshing out new and init commands

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bitcoin::{secp256k1::Secp256k1, XOnlyPublicKey};
 use bitcoin::util::bip32::ExtendedPrivKey;
 use anyhow::Result;
 use bip39::{self, Mnemonic};
+use chrono::format::format;
 // use bip85::*;
 use chrono::offset::Utc;
 use clap::{arg, Command};
@@ -19,70 +20,98 @@ use std::str::FromStr;
 use xdg::BaseDirectories;
 use nostr::{ Keys };
 mod bip85;
+use clap::Parser;
+#[allow(unreachable_code)]
+
+#[derive(Parser)] // requires `derive` feature
+#[command(author = "Matthias Debernardini <m.f.debern@protonmail.com>", version = "0.0.1", about = "CLI for the Osmium password manager service")]
+struct Cli {
+    #[arg(short = 'i')]
+    init: Option<bool>,
+    #[arg(short = 'n')]
+    new: Option<String>,
+    recover: Option<PathBuf>,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let u = User::new();
-    let _matches = Command::new("osmium")
-        .version("0.0.1")
-        .author("Matthias Debernardini <m.f.debern@protonmail.com>")
-        .arg(arg!(--init <BOOL> "path to load configuration file").default_missing_value("false"))
-        .arg(arg!(--new <INDEX_NAME> "like '0 titter' but with a space").default_missing_value(""))
-        .get_matches();
+
+    let args = Cli::parse();
+   
     init_log()?;
-    let new = "".to_owned();
+
+   match (args.init, args.recover) {
+    (None, None) => {
+        let a = BaseDirectories::new().unwrap();
+        let home = a.get_config_home();
+        let home = home.to_str();
+        let home = home.unwrap().to_string();
+        let data_path = format!("{}/osmium/mnemonic.backup", home);
+        let config_path = format!("{}/osmium/osmium.toml", home);
+        let xdg_data = a.find_data_file(data_path);
+        let xdg_config = a.find_config_file(config_path);
+        println!("basedir {a:?}");
+        println!("xdg_data {xdg_data:?}");
+        println!("xdg_config {xdg_config:?}");
+        todo!("check if path to config exists, then check for args.new");
+    },
+    (None, Some(p)) => todo!("check path for seed, warn user about putting seed in file then providing path to file, then ask for registration"),
+    (Some(_), None) => todo!("initialize new user, then ask for registration"),
+    (Some(_), Some(_)) => todo!("return error, can't be both"),
+  };
     // matches. .value_of("new").expect("Could not get new option");
-    let mut make_new: bool = false;
-    if make_new {}
-    let mut name = "";
-    let mut index = 0;
-    if new.is_empty() {
-        make_new = false;
-    } else {
-        make_new = true;
-        index = new
-            .split_ascii_whitespace()
-            .next()
-            .expect("Invalid")
-            .parse::<usize>()
-            .expect("Expected a number like 1 but could not get it");
-        name = new
-            .split_ascii_whitespace()
-            .last()
-            .expect("Expected a name like 'twitter' but could not get it");
-    }
-    let init_app = true;
-    // matches
-    //     .value_of("init")
-    //     .expect("Could not get init option")
-    //     .parse::<bool>()
-    //     .expect("Could not get true or false");
-    if init_app {
-        let seed: Vec<u8> = rand::thread_rng().sample_iter(&Standard).take(32).collect();
-        let mnemonic = bip39::Mnemonic::from_entropy(seed.as_ref())
-            .expect("Could not make mnemonic")
-            .to_string();
-        info!("the following data needs to be backed up to paper with pencil");
-        info!("without this data, it is impossible to recover your passwords");
-        info!("{mnemonic:?}");
-        let path = format!("/mnemonic.backup");
-        let f = File::create(path).expect("Unable to create file");
-        let mut f = BufWriter::new(f);
-        f.write_all(mnemonic.as_bytes())
-            .expect("Unable to write data");
-    }
-    if make_new {
-        // avoids accidentaly using these keys in wallet software
-        let network = bitcoin::Network::Regtest;
-        let path = format!("/mnemonic.backup");
-        let contents = fs::read_to_string(path)
-            .expect("Something went wrong when reading the mnemonic backup");
-        let seed = bip39::Mnemonic::parse_in_normalized(bip39::Language::English, contents.as_str())?
-            .to_entropy();
-        let root = ExtendedPrivKey::new_master(network, &seed)?;
-        info!("Root key: {}", root);
-        let new_password = get_new_password(root, index)?;
-        info!("new password for {name} with index {index}:");
-        info!("{new_password}");
-    }
+    // let mut make_new: bool = false;
+    // if make_new {}
+    // let mut name = "";
+    // let mut index = 0;
+    // if new.is_empty() {
+    //     make_new = false;
+    // } else {
+    //     make_new = true;
+    //     index = new
+    //         .split_ascii_whitespace()
+    //         .next()
+    //         .expect("Invalid")
+    //         .parse::<usize>()
+    //         .expect("Expected a number like 1 but could not get it");
+    //     name = new
+    //         .split_ascii_whitespace()
+    //         .last()
+    //         .expect("Expected a name like 'twitter' but could not get it");
+    // }
+    // let init_app = true;
+    // // matches
+    // //     .value_of("init")
+    // //     .expect("Could not get init option")
+    // //     .parse::<bool>()
+    // //     .expect("Could not get true or false");
+    // if init_app {
+    //     let seed: Vec<u8> = rand::thread_rng().sample_iter(&Standard).take(32).collect();
+    //     let mnemonic = bip39::Mnemonic::from_entropy(seed.as_ref())
+    //         .expect("Could not make mnemonic")
+    //         .to_string();
+    //     info!("the following data needs to be backed up to paper with pencil");
+    //     info!("without this data, it is impossible to recover your passwords");
+    //     info!("{mnemonic:?}");
+    //     let path = format!("/mnemonic.backup");
+    //     let f = File::create(path).expect("Unable to create file");
+    //     let mut f = BufWriter::new(f);
+    //     f.write_all(mnemonic.as_bytes())
+    //         .expect("Unable to write data");
+    // }
+    // if make_new {
+    //     // avoids accidentaly using these keys in wallet software
+    //     let network = bitcoin::Network::Regtest;
+    //     let path = format!("/mnemonic.backup");
+    //     let contents = fs::read_to_string(path)
+    //         .expect("Something went wrong when reading the mnemonic backup");
+    //     let seed = bip39::Mnemonic::parse_in_normalized(bip39::Language::English, contents.as_str())?
+    //         .to_entropy();
+    //     let root = ExtendedPrivKey::new_master(network, &seed)?;
+    //     info!("Root key: {}", root);
+    //     let new_password = get_new_password(root, index)?;
+    //     info!("new password for {name} with index {index}:");
+    //     info!("{new_password}");
+    // }
     Ok(())
 }
 
@@ -112,6 +141,7 @@ fn init_log() -> Result<(), Box<dyn Error>> {
         .apply()?)
 }
 
+#[derive(Debug)]
 struct User {
     config: PathBuf,
     registered: bool,
@@ -126,6 +156,7 @@ trait New {
     ///
     /// This function will return an error if .
     fn configure() -> Result<PathBuf, Box<dyn Error>>;
+    fn recover() -> Self;
     fn get_pubkey(mnemonic: &Mnemonic) -> XOnlyPublicKey;
     fn make_seed() -> String;
     fn new() -> Self;
@@ -184,6 +215,10 @@ impl New for User {
             mnemonic,
             pubkey,
         }
+    }
+
+    fn recover() -> Self {
+        todo!()
     }
 }
 
